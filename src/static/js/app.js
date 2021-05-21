b49c54d972ae1abd28173c415a9270ad6823eb9f
Darko <ddi@nebb.com>
2021-05-21 14:18:51 +0200

    izmeni vo app.js test na bind mount vo docker

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Nesto Tamu a'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
