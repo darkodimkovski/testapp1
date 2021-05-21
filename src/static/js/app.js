0056a5f094a095eb557f4b3586e04ea496967698
Darko <ddi@nebb.com>
2021-05-21 14:19:37 +0200

    add nesto button

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Nesto Tamu a'}
+                        {submitting ? 'Adding...' : 'Add Nesto'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
