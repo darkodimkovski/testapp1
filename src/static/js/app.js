99fc401ddb3fb7fd9b7358a64aa1b8547abec62a
Darko <ddi@nebb.com>
2021-05-21 14:21:02 +0200

    promena na add button

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Nesto'}
+                        {submitting ? 'Adding...' : 'Add Nesto tamu izmena'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
